5b22c49add41e8ec531d14bcea6b3d5505fa8361
lelani82 <lelaniparker@hotmail.com>
2020-01-06 21:39:48 +1000

    database branch started

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const express = require("express")
 const cors = require("cors") // Enabling application middleware
 const bodyParser = require("body-parser")
 const dataRouter = require("./routes/data_routes") // Set up routing
+const mongoose = require("mongoose");
 
 const port = 3000 // Routing to listen on port 3000
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+const dbConn = "mongodb://localhost/analyzeVit"
+// Set three properties to avoid deprecation warnings:
+// useNewUrlParser: true
+// useUnifiedTopology: true
+// useFileAndModify: false
+mongoose.connect(
+	dbConn,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	},
+	err => {
+		if (err) {
+			console.log("Error connecting to database", err)
+		} else {
+			console.log("Connected to database!")
+		}
+	}
+)
 
 app.use("/data", dataRouter)
 
