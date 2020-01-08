2b15de5dc0fa64c013da5a03cd0df244b0a03989
Tony <huynh.tony55@gmail.com>
2020-01-08 22:12:24 +1000

    started loadJson

---
+++ helpers/loadJson.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const assert = require("assert");
+const Product = require("../models/product");
+const products = require("../data/vitaminProducts.json");
+
+moongoose.connect('mongodb://localhost/analyzeVit');
+
+const db = mongoose.connection;
+
+const importProducts = () => {
+    Product.collection.insertMany(products, (err, res) => {
+        assert.equal(null, err);
+        assert.equal(4, res.insertedCount);
+        db.close();
+    });
+    console.log(`Products data imported successfully ${Product.find()}`);
+}
+
+importProducts();
