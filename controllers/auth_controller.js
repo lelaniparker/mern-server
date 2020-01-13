670dc5ad599443e5d12dfdb51473aacee5cc0a5d
lelani82 <lelaniparker@hotmail.com>
2020-01-13 11:17:40 +1000

    authentication user login completed and tested

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -13,15 +13,28 @@ const register = function (req, res) {
             });
         } else {
             // Log in the newly registered user
-            passport.authenticate('local')(req, res, function () {
-                console.log('authenticated', req.user.username);
-                console.log('session object:', req.session);
-                console.log('req.user:', req.user);
-                res.status(200);
-                res.json(req.user);
-            });
+            loginUser(req, res);
         }
     });
 };
 
-module.exports = { register };
+// helper functions
+const authenticate = passport.authenticate('local');
+
+function loginUser(req, res) {
+    // passport.authenticate returns a function that we will call with req, res, and a callback function to execute on success
+
+    authenticate(req, res, function () {
+        console.log('authenticated', req.user.username);
+        console.log('session object:', req.session);
+        console.log('req.user:', req.user);
+        res.status(200);
+        res.json(req.user);
+    });
+}
+
+module.exports = {
+    register,
+    login: loginUser,
+    //logout
+};
