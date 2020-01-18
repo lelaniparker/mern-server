e204cc36a5f5bb397bfb83e2b70ba9a93cce20e2
BlueCodeThree <carliemhamilton@gmail.com>
2020-01-18 13:58:05 +1000

    updated register function

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,19 +2,40 @@ const passport = require("passport");
 const User = require("../models/user");
 
 const register = function (req, res) {
-    User.register(new User({
-        username: req.body.username,
-        email: req.body.email
-    }), req.body.password, function (err) {
-        if (err) {
-            res.status(500);
-            res.json({
-                error: err
-            });
-        } else {
-            // Log in the newly registered user
-            loginUser(req, res);
-        }
+  User.register(new User({
+      username: req.body.username,
+      email: req.body.email,
+      role: req.body.role || 'user'
+  }), req.body.password, function (err) {
+      if (err) {
+          console.log(err)
+          if(err.name === 'UserExistsError') {
+              res.status(409)
+              res.json({
+                  error: err.message
+              });
+          } else {
+              res.status(500);
+              res.json({
+                  error: err
+              });
+          }
+      } else {
+          // Log in the newly registered user
+          loginUser(req, res);
+      }
+  });
+};
+
+function loginUser(req, res) {
+    // passport.authenticate returns a function that we will call with req, res, and a callback function to execute on success
+
+    authenticate(req, res, function () {
+        console.log('authenticated', req.user.username);
+        console.log('session object:', req.session);
+        console.log('req.user:', req.user);
+        res.status(200);
+        res.json(req.user);
     });
 };
 
@@ -28,18 +49,6 @@ const logout = function (req, res) {
 
 // helper functions
 const authenticate = passport.authenticate('local');
-
-function loginUser(req, res) {
-    // passport.authenticate returns a function that we will call with req, res, and a callback function to execute on success
-
-    authenticate(req, res, function () {
-        console.log('authenticated', req.user.username);
-        console.log('session object:', req.session);
-        console.log('req.user:', req.user);
-        res.status(200);
-        res.json(req.user);
-    });
-};
 
 module.exports = {
     register,
